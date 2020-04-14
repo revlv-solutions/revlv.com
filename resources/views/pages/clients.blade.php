0a9070d5cee00b6b5fa15f5964353d7d462398d4
Ardee Poblete <pobleteardee@yahoo.com>
2020-04-14 10:10:41 +0800

    added clients logo

--- resources/views/pages/clients.blade.php
+++ resources/views/pages/clients.blade.php
@@ -13,147 +13,147 @@
       </div>
       <div class="content__block__right">
         <div class="clients">
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/afp-logo.png">
             </span>
             <span class="clients__item__name">Armed Forces of The Philippines</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/astoria-current-logo.png">
             </span>
             <span class="clients__item__name">Astoria Current</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/astoria-plaza-logo.png">
             </span>
             <span class="clients__item__name">Astoria Plaza</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/astoria-boracay-logo.png">
             </span>
             <span class="clients__item__name">Astoria Boracay</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/astoria-greenbelt-logo.png">
             </span>
             <span class="clients__item__name">Astoria Greenbelt</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/astoria-palawan-logo.png">
             </span>
             <span class="clients__item__name">Astoria Palawan</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/adnu-logo.png">
             </span>
             <span class="clients__item__name">Ateneo de Naga University</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/avenue-plaza-logo.png">
             </span>
             <span class="clients__item__name">Avenue Plaza Hotel</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/belo-logo.png">
             </span>
             <span class="clients__item__name">Belo Medical Group</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/brent-logo.png">
             </span>
             <span class="clients__item__name">Brent International School (Calamba, Laguna)</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/navotas-logo.png">
             </span>
             <span class="clients__item__name">City of Navotas</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/csa-logo.png">
             </span>
             <span class="clients__item__name">Colegio de San Agustin (Biñan, Laguna)</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/coa-logo.png">
             </span>
             <span class="clients__item__name">Commission on Audit</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/crimson-logo.png">
             </span>
             <span class="clients__item__name">Crimson Hotel</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/bucor-logo.png">
             </span>
             <span class="clients__item__name">DOJ-BuCor (Bureau of Corrections)</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/mrt-logo.png">
             </span>
             <span class="clients__item__name">DOTR-MRT3 Line 3</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/seton-logo.png">
             </span>
             <span class="clients__item__name">St.Elizabeth Seton School</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/miriam-logo.png">
             </span>
             <span class="clients__item__name">Miriam College (Sta. Rosa Laguna)</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
             <img src="/img/logo-default.png">
             </span>
             <span class="clients__item__name">Oriental Consultants Global</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/pma-logo.png">
             </span>
             <span class="clients__item__name">Philippine Military Academy</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/navy-logo.png">
             </span>
             <span class="clients__item__name">Philippine Navy</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
             <img src="/img/logo-default.png">
             </span>
             <span class="clients__item__name">Pinnacle Hotel and Resort Boracay</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/marina-logo.png">
             </span>
             <span class="clients__item__name">Maritime Industry Authority of the Philippines</span>
           </a>
-          <a href="" class="clients__item">
+          <a class="clients__item">
             <span class="clients__item__image">
-            <img src="/img/logo-default.png">
+            <img src="/img/villa-caceres-logo.png">
             </span>
             <span class="clients__item__name">Villa Caceres Hotel</span>
           </a>
